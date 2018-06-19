8ce48bd6f101c7ed32260d49dbe08c4cd967a3ef
zouhang <1102639563@qq.com>
2018-06-19 21:57:47 +0800

    mand-mobile test

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 import lazyload from 'vue-lazyload'
+import mandMobile from 'mand-mobile'
+Vue.use(mandMobile)
 
 // 引入css
 import './common/stylus/index.styl'
